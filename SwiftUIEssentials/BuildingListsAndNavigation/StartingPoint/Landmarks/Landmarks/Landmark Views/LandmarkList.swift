3c50b566bff081b5e02249c6effadeb689f63174
clarknt <24315769+clarknt@users.noreply.github.com>
2019-07-30 00:23:22 -0400

    Building lists and navigation

---
+++ SwiftUIEssentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/Landmark Views/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by clarknt on 2019-07-29.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
