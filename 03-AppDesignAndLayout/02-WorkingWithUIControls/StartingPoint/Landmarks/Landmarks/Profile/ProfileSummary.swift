81bcf665a06e2b24718df862f195c40657da8669
clarknt <24315769+clarknt@users.noreply.github.com>
2019-08-23 15:02:45 -0400

    Workaround for HikeView functionnalities in profile

--- 03-AppDesignAndLayout/02-WorkingWithUIControls/StartingPoint/Landmarks/Landmarks/Profile/ProfileSummary.swift
+++ 03-AppDesignAndLayout/02-WorkingWithUIControls/StartingPoint/Landmarks/Landmarks/Profile/ProfileSummary.swift
@@ -18,50 +18,64 @@ struct ProfileSummary: View {
     }
     
     var body: some View {
-        List {
-            Text(profile.username)
-                .bold()
-                .font(.title)
-            
-            Text("Notifications: \(self.profile.prefersNotifications ? "On": "Off" )")
-            
-            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
-            
-            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
-            
+        // List present in tutorial breaks the HikeView functionalities
+        // like in Home, as a workaround, simulate a List with ScrollView + VStack
+        ScrollView(.vertical, showsIndicators: true) {
             VStack(alignment: .leading) {
-                Text("Completed Badges")
-                    .font(.headline)
-                ScrollView {
-                    HStack {
-                        HikeBadge(name: "First Hike")
-                        
-                        HikeBadge(name: "Earth Day")
-                            .hueRotation(Angle(degrees: 90))
-                        
-                        
-                        HikeBadge(name: "Tenth Hike")
-                            .grayscale(0.5)
-                            .hueRotation(Angle(degrees: 45))
-                    }
-                }
-                .frame(height: 140)
-            }
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Divider()
+                
+                Text("Notifications: \(self.profile.prefersNotifications ? "On": "Off" )")
+                
+                Divider()
 
-            VStack(alignment: .leading) {
-                Text("Recent Hikes")
-                    .font(.headline)
-            
-                // Within a List (top element in the body) like in the turorial,
-                // Heart Rate and Pace buttons don't work (clicking anywhere
-                // has the same effect as the toggle button).
-                // In a ScrollView + VStack instead of the list, they work
-                // (but then the layout is different).
-                // The toggle button color is also off.
-                HikeView(hike: hikeData[0])
+                Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+                
+                Divider()
+
+                Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    ScrollView {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            
+                            
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                    }
+                    .frame(height: 140)
+                    
+                    
+                    Divider()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    // Within a List (top element in the body) like in the turorial,
+                    // Heart Rate and Pace buttons don't work (clicking anywhere
+                    // has the same effect as the toggle button).
+                    // The toggle button color is also off.
+                    // In a ScrollView + VStack instead of the list, they work.
+                    HikeView(hike: hikeData[0])
+
+                    Divider()
+                }
             }
-            // turn off animation for the title or it goes over the other elements
-            .animation(nil)
         }
     }
 }
